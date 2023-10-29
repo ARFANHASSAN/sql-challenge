c950d7d367359721843109bb30cf552a7164c74a
ARFAN HASSAN <arfanhassan@gmail.com>
2023-10-29 13:36:57 +0000

    updated assignment

--- EmployeeSQL/Starter_Code (3)/Starter_Code/Employees_schemata.sql
+++ EmployeeSQL/Starter_Code (3)/Starter_Code/Employees_schemata.sql
@@ -1,56 +1,88 @@
 --Drop table if exist
-DROP TABLE titles;
+DROP TABLE departments CASCADE;
+CREATE TABLE "departments" (
+   "dept_no" VARCHAR   NOT NULL,
+   "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
 
+--Drop table if exist
+DROP TABLE titles CASCADE;
 -- create titles table
-CREATE TABLE titles (
-	title_id varchar(5) PRIMARY KEY,
-	title varchar(30)
+CREATE TABLE "titles" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
-select*from titles;
---Drop table if exist
-DROP TABLE employees;
--- create employees table
-CREATE TABLE employees (
-	emp_no varchar(10) PRIMARY KEY,
-	emp_title_id varchar(5) references titles(title_id),
-	birth_date date,
-	first_name varchar(45),
-	last_name varchar(45),
-	sex varchar(1),
-	hire_date date
-);
-select*from employees;
---Drop table if exist
-DROP TABLE departments;
--- create departments table
-CREATE TABLE departments (
-	dept_no varchar(5) PRIMARY KEY,
-	dept_name varchar(30)
-);
-select*from departments;
---Drop table if exist
-DROP TABLE dept_manager;
--- create department managers table
-CREATE TABLE dept_manager (
-	dept_no varchar(5) references departments(dept_no),
-	emp_no varchar(10)
-	
-);
-select*from dept_manager;
 
 --Drop table if exist
-DROP TABLE dept_emp;
--- create department employees table
-CREATE TABLE dept_emp (
-	emp_no varchar(10) references employees(emp_no),
-	dept_no varchar(5) references departments(dept_no)
+DROP TABLE employees CASCADE;
+-- create employees table
+CREATE TABLE "employees" (
+   "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
-select*from dept_emp;
+
 --Drop table if exist
-DROP TABLE salaries;
--- create salaries table
-CREATE TABLE salaries (
-	emp_no varchar(10) references employees(emp_no),
-	salary int
+DROP TABLE dept_emp CASCADE;
+-- create department employees table
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL
 );
-select*from salaries;
+
+--Drop table if exist
+DROP TABLE dept_manager CASCADE;
+-- create department managers table
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+--Drop table if exist
+DROP TABLE salaries CASCADE;
+-- create salaries table
+CREATE TABLE "salaries" (
+  "emp_no" INT   NOT NULL,
+  "salary" INT   NOT NULL
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+
+SELECT * FROM departments;
+SELECT * FROM titles;	
+SELECT * FROM employees;	
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;	
+SELECT * FROM salaries;
+
+
--- EmployeeSQL/Starter_Code (3)/Starter_Code/employees_queries.sql
+++ EmployeeSQL/Starter_Code (3)/Starter_Code/employees_queries.sql
@@ -1,3 +1,4 @@
+--DATA ANALYSIS
 -- employee number, last name, first name, sex, and salary
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
